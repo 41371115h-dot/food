ccde86310455bc51cb87f9093fad414c537f3d68
41371115h-dot <41371115h@gapps.ntnu.edu.tw>
2025-11-17 21:07:42 -0500

    Initialize Express server with /gemini endpoint
    Set up a basic Express server with CORS and a POST endpoint.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+//設定proxy server
+
+app.post('/gemini', async (req, res) => {
+    //呼叫google scripts
+    const response = await fetch("https://script.google.com/macros/s/AKfycbzQDmgTSxyPKTBPqEmW3okvpEmXwUUVwG-amxBGe5oVhSuO5dlzgFWgbnqzt69QjzEE/exec", {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(req.body)
+    })
+    const data = await response.text();
+    console.log(data)
+    res.json(JSON.parse(data)); //回傳JSON資料
+});
+
+//設定localhost在哪個port
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
